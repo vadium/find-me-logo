ab62715030ac5f6593c583af78ede08075b41af3
Vadium β <vabrva@gmail.com>
2013-03-08 12:34:34 +0100

    Impoved performance
    - Slightly other algorythm
    
    2DO:
    - show frames of results for demonstration
    - use any video as input

--- logo.cpp
+++ logo.cpp
@@ -4,13 +4,15 @@
 #include <cxcore.h>
 #include <cxtypes.h>
 #include <ml.h>
+#include <math.h>
 
 #define CV_IMWRITE_JPEG_QUALITY 1
 
 int main(int argc, char* argv[])
 {	
 	int flag1=0;
-	int i,j;
+	int i,j,dRed, dGreen, dBlue, dTotal;
+	const int fuMysticDigit = 66;
 	int p[3]; //params 4 save in jpg
 	p[0] = CV_IMWRITE_JPEG_QUALITY;
 	p[1] = 100;
@@ -18,27 +20,27 @@ int main(int argc, char* argv[])
 	unsigned int stupidc0unt=0;
 	IplImage* frame =0;
 	IplImage* resu = 0;
-	char* filename = argc >= 2 ? argv[1] : "sample.mp4";
+	const char* filename = argc >= 2 ? argv[1] : "sample.mp4";
 
-	cvNamedWindow("LOL", CV_WINDOW_AUTOSIZE);
-	
+	cvNamedWindow("captured", CV_WINDOW_AUTOSIZE);
+
 	CvCapture* capture = cvCreateFileCapture(filename);	
 
 	while(1){
 		frame = cvQueryFrame(capture);
 		if (!frame){break;}
-	
+
 		stupidc0unt++;
-		if(stupidc0unt/165==stupidc0unt%165)
+		if(stupidc0unt/99==stupidc0unt%99)
 			{
-				
+
 				if(flag1==0){
 					resu = cvCloneImage(frame);
+					flag1=1;
 				}
-								
+
 				int height     = frame->height;
 				int width      = frame->width;
-				int step       = frame->widthStep/sizeof(uchar);
 				int channels   = frame->nChannels;
 				uchar* data1    = (uchar *)frame->imageData;
 				uchar* data2    = (uchar *)resu->imageData;
@@ -47,25 +49,27 @@ int main(int argc, char* argv[])
 				{
 					for(j=0;j<width;j++)
 					{
-						if ((data1[i*step+j*channels+0]!=data2[i*step+j*channels+0])&&
-							(data1[i*step+j*channels+1]!=data2[i*step+j*channels+1])&&
-							(data1[i*step+j*channels+2]!=data2[i*step+j*channels+2]))
+						dRed	= (int)data1[i*width*channels+j*channels]-(int)data2[i*width*channels+j*channels];
+						dGreen	= (int)data1[i*width*channels+j*channels+1]-(int)data2[i*width*channels+j*channels+1];
+						dBlue	= (int)data1[i*width*channels+j*channels+2]-(int)data2[i*width*channels+j*channels+2];
+						dTotal	= sqrt(pow(dRed,2)+pow(dGreen,2)+pow(dBlue,2));
+
+						if (dTotal > fuMysticDigit)
 						{
-							data2[i*step+j*channels+0] = 0;
-							data2[i*step+j*channels+1] = 0;
-							data2[i*step+j*channels+2] = 0;
+							data2[i*width*channels+j*channels]   = 0;
+							data2[i*width*channels+j*channels+1] = 0;
+							data2[i*width*channels+j*channels+2] = 0;
 						}
 					}
 				}
-				cvShowImage( "LOL",resu);
-				flag1=1;				
-			}
+				cvShowImage( "captured",resu);
+				}
 		}
-	
+
 	cvSaveImage("result.jpg", resu, p);
 
 	cvReleaseCapture(&capture);
 
-	cvDestroyWindow("LOL");
+	cvDestroyWindow("captured");
         return 0;		
 }
